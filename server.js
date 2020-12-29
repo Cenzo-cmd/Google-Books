ebef9e3775bfacd6d6b5a4202edf6031581e4919
Vincent <vrdphone@gmail.com>
2020-12-29 16:34:30 -0600

    added routes and models

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mongoose = require("moongoose");
-const routes = require("./routes");
+const routes = require("./routes/htmlRoutes");
+const logger = require("morgan");
 const app = express();
 const PORT = process.env.PORT || 8081;
 
+app.use(logger("dev"))
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -11,9 +13,15 @@ if(process.env.NODE_ENV === "production") {
     app.use(express.static("client/build"));
 }
 
-app.use(routes);
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/google-books");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/vacationrental", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  });
 
 app.listen(PORT, () => {
     console.log(`Server is listening on http://localhost:${PORT}`)
