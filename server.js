753a47d533fdf02f898f0f59bc04adccdacc3f5d
Vincent <vrdphone@gmail.com>
2020-12-29 16:37:31 -0600

    fixed require error

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const mongoose = require("moongoose");
+const mongoose = require("mongoose");
 const routes = require("./routes/htmlRoutes");
 const logger = require("morgan");
 const app = express();
